5bc3437165b82de0e276bbed6d571f48029a2c99
ReginaGalvezCh <reginagalvezch@gmail.com>
2020-10-01 19:57:36 +0000

    adding hover delete

--- to-do-list/src/components/ListItems.js
+++ to-do-list/src/components/ListItems.js
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Listitems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import FlipMove from 'react-flip-move';
 
 function ListItems(props) {
     const items = props.items;
+    const [inHover, setHover] = useState(false);
     const listItems = items.map(item => {
-        return <div className="list" key={item.key}>
-            <p >{item.text}  <span className="hover">
-                <FontAwesomeIcon className="faicons" icon='trash'
-                    onClick={() => props.deleteItem(item.key)
-                    }></FontAwesomeIcon>
-            </span>
+        return <div className="list" key={item.key}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)} >
+            <p >{item.text}
+                {inHover && <span className="hover">
+                    <FontAwesomeIcon className="faicons" icon='trash'
+                        onClick={() => props.deleteItem(item.key)
+                        }></FontAwesomeIcon>
+                </span>}
+
             </p>
 
         </div>
